b532c945e2f4e1edb2d6f6b7f782deac00d67e84
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-19 04:20:40 -0700

    add UserEntity to app

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,14 @@
+import { Base } from 'src/common/entities/base-entity';
+import { Column, Entity } from 'typeorm';
 
+@Entity('user')
+export class UserEntity extends Base {
+  @Column({ type: 'varchar', nullable: false })
+  fullName: string;
+
+  @Column({ type: 'varchar', unique: true })
+  mobile: string;
+
+  @Column({ type: 'double', default: 0 })
+  balance: number;
+}
