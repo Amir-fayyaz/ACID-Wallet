19d45c40bd8f7b3fbe8f8fdf440840cefc5c8bd1
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-19 04:44:53 -0700

    add getUserById service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly UserRepository: Repository<UserEntity>,
+  ) {}
+
+  public async getUserById(id: number) {
+    const user = await this.UserRepository.findOne({ where: { id } });
+
+    if (!user) throw new NotFoundException('user not found');
+
+    return user;
+  }
 }
