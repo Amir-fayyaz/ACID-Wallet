2b0cc96ad47b2d872a5835ed387a9436219dbb11
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-19 04:45:06 -0700

    add getUserById api

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('api/v1/users')
 export class UserController {
-  constructor() {}
+  constructor(private readonly UserService: UserService) {}
+
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) userId: number) {
+    return await this.UserService.getUserById(userId);
+  }
 }
