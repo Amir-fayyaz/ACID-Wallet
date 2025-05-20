21dc326a11d508d9a23a0dc881a2d4eab6d86fa6
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-20 06:39:25 -0700

    add withdraw api to app

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { DepositDto } from './dto/deposit.dto';
+import { WithDrawDto } from './dto/withdraw.dto';
 
 @Controller('api/v1/wallet')
 export class WalletController {
@@ -9,5 +10,10 @@ export class WalletController {
   @Post('deposit')
   async deposit(@Body() data: DepositDto) {
     return await this.WalletService.deposit(data);
+  }
+
+  @Post('withdraw')
+  async withDraw(@Body() data: WithDrawDto) {
+    return await this.WalletService.withdraw(data);
   }
 }
