1e441022421e2fdcd8aaa2633a87e83736003dd4
Amir-Fayyaz <fayamir54@gmail.com>
2025-05-20 06:38:25 -0700

    add fake product-data for test

---
+++ src/common/data/product.data.ts
@@ -0,0 +1,48 @@
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export const Products: IProduct[] = [
+  {
+    id: 1,
+    name: 'product 1',
+    price: 13_000,
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    price: 130_000,
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    price: 120_600,
+  },
+  {
+    id: 4,
+    name: 'product 4',
+    price: 160_000,
+  },
+  {
+    id: 5,
+    name: 'product 5',
+    price: 1_213_000,
+  },
+  {
+    id: 6,
+    name: 'product 6',
+    price: 1_234_000,
+  },
+  {
+    id: 7,
+    name: 'product 7',
+    price: 50_000,
+  },
+  {
+    id: 8,
+    name: 'product 8',
+    price: 98_000,
+  },
+];
